efd7da6acdac302cc5a3186e5db71367d9bc1e17
Hassan Sheikh <hassansheikh9p9@gmail.com>
2024-08-08 10:21:42 +0500

    config for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -11,55 +11,60 @@ import { AllProductsLoader } from "./Pages/AllProductsPage.tsx";
 import { singleProductLoader } from "./Pages/ProductDescriptionPage.tsx";
 import { cartLoader } from "./Pages/CartPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorElement />,
+      children: [
+        {
+          index: true,
+          Component: lazy(() => import("./Pages/HomePage")),
+        },
+        {
+          path: "/shop",
+          loader: () => redirect("/shop/category/all"),
+        },
+        {
+          path: "/shop/category",
+          loader: () => redirect("/shop/category/all"),
+        },
+        {
+          path: "/shop/category/all",
+          loader: AllProductsLoader,
+          Component: lazy(() => import("./Pages/AllProductsPage.tsx")),
+        },
+        {
+          path: "/shop/category/:categoryName",
+          loader: CategoryProductsLoader,
+          Component: lazy(() => import("./Pages/CategoryProductsPage.tsx")),
+        },
+        {
+          path: "/shop/category/:categoryName/:productId",
+          loader: singleProductLoader,
+          Component: lazy(() => import("./Pages/ProductDescriptionPage.tsx")),
+        },
+        {
+          path: "/cart",
+          loader: cartLoader,
+          Component: lazy(() => import("./Pages/CartPage")),
+        },
+        {
+          path: "/contactUs",
+          Component: lazy(() => import("./Pages/ContactUsPage")),
+        },
+        {
+          path: "/auth",
+          Component: lazy(() => import("./Pages/AuthenticationPage")),
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <ErrorElement />,
-    children: [
-      {
-        index: true,
-        Component: lazy(() => import("./Pages/HomePage")),
-      },
-      {
-        path: "/shop",
-        loader: () => redirect("/shop/category/all"),
-      },
-      {
-        path: "/shop/category",
-        loader: () => redirect("/shop/category/all"),
-      },
-      {
-        path: "/shop/category/all",
-        loader: AllProductsLoader,
-        Component: lazy(() => import("./Pages/AllProductsPage.tsx")),
-      },
-      {
-        path: "/shop/category/:categoryName",
-        loader: CategoryProductsLoader,
-        Component: lazy(() => import("./Pages/CategoryProductsPage.tsx")),
-      },
-      {
-        path: "/shop/category/:categoryName/:productId",
-        loader: singleProductLoader,
-        Component: lazy(() => import("./Pages/ProductDescriptionPage.tsx")),
-      },
-      {
-        path: "/cart",
-        loader: cartLoader,
-        Component: lazy(() => import("./Pages/CartPage")),
-      },
-      {
-        path: "/contactUs",
-        Component: lazy(() => import("./Pages/ContactUsPage")),
-      },
-      {
-        path: "/auth",
-        Component: lazy(() => import("./Pages/AuthenticationPage")),
-      },
-    ],
-  },
-]);
+    basename: "React-Ecommerce/",
+  }
+);
 
 function App() {
   return <RouterProvider router={router}></RouterProvider>;
