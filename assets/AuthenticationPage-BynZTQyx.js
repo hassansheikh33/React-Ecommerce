c2f6752a96526bedce2fa23adb43e3e15fafc563
Hassan Sheikh <hassansheikh9p9@gmail.com>
2024-08-09 14:26:17 +0500

    Updates

--- assets/AuthenticationPage-BynZTQyx.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-yE8b53F9.js";const n={};function e(){return t.jsx("div",{children:t.jsx("h1",{className:n.heading,children:"Authentication Page"})})}function s(){return t.jsx(e,{})}export{s as default};
